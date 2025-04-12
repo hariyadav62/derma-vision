1a9fb7c3ea3a003b490752b01b93168476d65b09
hariyadav62 <rockstarhariyadav@gmail.com>
2025-04-12 19:43:40 +0530

    changed url

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://vision-api.onrender.com";
 
 const imageInput = document.getElementById("imageInput");
 const previewImage = document.getElementById("previewImage");
